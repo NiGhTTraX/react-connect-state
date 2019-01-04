e3778c08b60be7253e18bb9556a27b2a6f3a6150
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-01-04 21:12:57 +0100

    Add simple implementation of connectToState

--- src/connect.tsx
+++ src/connect.tsx
@@ -1,3 +1,25 @@
-export default function connectToState() {
+import React, { Component, ComponentType } from 'react';
+import StateContainer from './state-container';
+import { Omit } from 'react-bind-component';
 
+export default function connectToState<
+  ViewProps extends Record<K, StateContainer<State>>,
+  State,
+  K extends string
+>(
+  View: ComponentType<ViewProps>,
+  container: StateContainer<State>,
+  prop: K
+): ComponentType<Omit<ViewProps, K>> {
+  return class ConnectedView extends Component<Omit<ViewProps, K>> {
+    render() {
+      const props = {
+        ...this.props,
+        [prop]: container
+      };
+
+      // @ts-ignore
+      return <View {...props} />;
+    }
+  };
 }
--- tests/node/connect.spec.tsx
+++ tests/node/connect.spec.tsx
@@ -1,8 +1,27 @@
-import { describe, it } from './suite';
+import React from 'react';
+import { Mock } from 'typemoq';
+import { $render, describe, expect, it } from './suite';
 import connectToState from '../../src';
+import StateContainer from '../../src/state-container';
+import { createReactStub } from 'react-mock-component';
 
 describe('connectToState', () => {
-  it('should work', () => {
-    connectToState();
+  it('should pass the container on the first render', () => {
+    interface FooState {
+      bar: number;
+    }
+
+    const fooContainer = Mock.ofType<StateContainer<FooState>>();
+
+    interface ViewProps {
+      foo: StateContainer<FooState>;
+    }
+
+    const View = createReactStub<ViewProps>();
+    const ConnectedView = connectToState(View, fooContainer.object, 'foo');
+
+    $render(<ConnectedView />);
+
+    expect(View.renderedWith({ foo: fooContainer.object })).to.be.true;
   });
 });
