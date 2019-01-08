0db2efce88fc48469cd5abdc784ee208c772390e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-01-08 10:55:53 +0100

    Remove `undefined` type from state
    Consumers of the container should rely on the state always being
    defined. With this definitely assigned assertion we break that contract
    and instead rely on the derived class to make sure the state is init.
    The compiler can't check that for us so this will not prevent runtime
    errors, but it is an improvement in the API syntax.

--- src/state-container.tsx
+++ src/state-container.tsx
@@ -3,7 +3,9 @@ type Listener = () => void;
 export default abstract class StateContainer<T> {
   private listeners: Listener[] = [];
 
-  state: T | undefined;
+  // This can result in runtime errors if the derived class doesn't
+  // initialize the state. TODO: how can we prevent this?
+  state!: T;
 
   protected setState(partialState: Partial<T>) {
     this.state = Object.assign({}, this.state, partialState);
