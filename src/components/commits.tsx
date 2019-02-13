fa53d0148321dced2f7a30d88b4b17bc2e140f79
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-02-13 14:52:14 +0100

    Render the commits like a branch

--- src/components/commits.tsx
+++ src/components/commits.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import { ICommitsContainer } from '../commits-container';
+import { ICommitsContainer, StateCommit } from '../commits-container';
+import './commits.less';
 
 export interface CommitsProps {
   commits: ICommitsContainer
@@ -10,13 +12,25 @@ export default class Commits extends Component<CommitsProps> {
     const { master } = this.props.commits.state;
 
     return <div>
-      <ul className="master">
-        {/* TODO: introduce commit IDs */}
-        {/* eslint-disable-next-line react/no-array-index-key */}
-        {master.map((commit, i) => <li className="commit" key={i}>
-          Commit
-        </li>)}
-      </ul>
+      {this.connectCommits(master)}
     </div>;
+  }
+
+  private connectCommits(commits: StateCommit[]) {
+    const connectedCommits: any[] = [];
+
+    commits.forEach((c, i) => {
+      // TODO: introduce commit IDs
+      connectedCommits.push(
+        // eslint-disable-next-line react/no-array-index-key
+        <li className="commit" key={`commit${i}`} />,
+        // eslint-disable-next-line react/no-array-index-key
+        <li className="commit-divider" key={`divider${i}`} />
+      );
+    });
+
+    return <ul className="master">
+      {connectedCommits.slice(0, -1)}
+    </ul>;
   }
 }
