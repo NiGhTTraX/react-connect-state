0f40c61e34a6336d344a0f6fd2045bebc1494ff6
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-02-14 14:32:31 +0100

    Rename tests

--- tests/node/components/commits.spec.tsx
+++ tests/node/components/commits.spec.tsx
@@ -5,71 +5,73 @@ import Commits, { CommitProps } from '../../../src/components/commits';
 import { ICommitsContainer, StateCommit } from '../../../src/commits-container';
 
 describe('Commits', () => {
-  const commit1: StateCommit = {
-    id: 1,
-    state: { foo: 'bar' },
-    checkout: () => {},
-    parent: null,
-    instance: { state: { foo: 'bar' } }
-  };
-  const commit2: StateCommit = {
-    id: 2,
-    state: { foo: 'baz' },
-    checkout: () => {},
-    parent: commit1,
-    instance: { state: { foo: 'baz' } }
-  };
-  const commit3: StateCommit = {
-    id: 3,
-    state: { foo: 'gaga' },
-    checkout: () => {},
-    parent: commit2,
-    instance: { state: { foo: 'gaga' } }
-  };
-
-  it('should render all the master commits', () => {
-    const commits: ICommitsContainer = {
-      state: {
-        master: [commit1, commit2, commit3],
-        branches: [],
-        detached: false,
-        head: commit3.id
-      },
-      reset: () => {}
+  describe('master', () => {
+    const commit1: StateCommit = {
+      id: 1,
+      state: { foo: 'bar' },
+      checkout: () => {},
+      parent: null,
+      instance: { state: { foo: 'bar' } }
+    };
+    const commit2: StateCommit = {
+      id: 2,
+      state: { foo: 'baz' },
+      checkout: () => {},
+      parent: commit1,
+      instance: { state: { foo: 'baz' } }
+    };
+    const commit3: StateCommit = {
+      id: 3,
+      state: { foo: 'gaga' },
+      checkout: () => {},
+      parent: commit2,
+      instance: { state: { foo: 'gaga' } }
     };
 
-    const Commit = createReactStub<CommitProps>();
+    it('should render all the commits', () => {
+      const commits: ICommitsContainer = {
+        state: {
+          master: [commit1, commit2, commit3],
+          branches: [],
+          detached: false,
+          head: commit3.id
+        },
+        reset: () => {}
+      };
 
-    $render(<Commits
-      Commit={Commit}
-      commits={commits}
-    />);
+      const Commit = createReactStub<CommitProps>();
 
-    expect(Commit.renderedWith({ commit: commit1 })).to.be.true;
-    expect(Commit.renderedWith({ commit: commit2 })).to.be.true;
-    expect(Commit.renderedWith({ commit: commit3 })).to.be.true;
-  });
+      $render(<Commits
+        Commit={Commit}
+        commits={commits}
+      />);
 
-  it('should mark checkouts on master', () => {
-    const commits: ICommitsContainer = {
-      state: {
-        master: [commit1, commit2, commit3],
-        branches: [],
-        detached: false,
-        head: commit2.id
-      },
-      reset: () => {}
-    };
+      expect(Commit.renderedWith({ commit: commit1 })).to.be.true;
+      expect(Commit.renderedWith({ commit: commit2 })).to.be.true;
+      expect(Commit.renderedWith({ commit: commit3 })).to.be.true;
+    });
 
-    const Commit = createReactStub<CommitProps>();
+    it('should mark commits after a checkout', () => {
+      const commits: ICommitsContainer = {
+        state: {
+          master: [commit1, commit2, commit3],
+          branches: [],
+          detached: false,
+          head: commit2.id
+        },
+        reset: () => {}
+      };
 
-    $render(<Commits
-      Commit={Commit}
-      commits={commits}
-    />);
+      const Commit = createReactStub<CommitProps>();
 
-    expect(Commit.renderedWith({ commit: commit1, disabled: false })).to.be.true;
-    expect(Commit.renderedWith({ commit: commit2, disabled: false })).to.be.true;
-    expect(Commit.renderedWith({ commit: commit3, disabled: true })).to.be.true;
+      $render(<Commits
+        Commit={Commit}
+        commits={commits}
+      />);
+
+      expect(Commit.renderedWith({ commit: commit1, disabled: false })).to.be.true;
+      expect(Commit.renderedWith({ commit: commit2, disabled: false })).to.be.true;
+      expect(Commit.renderedWith({ commit: commit3, disabled: true })).to.be.true;
+    });
   });
 });
