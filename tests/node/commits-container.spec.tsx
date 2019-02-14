e0256d69455423acedfe5abc3d94cd2e0002a09d
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-02-14 21:05:14 +0100

    Reorder tests

--- tests/node/commits-container.spec.tsx
+++ tests/node/commits-container.spec.tsx
@@ -8,27 +8,8 @@ describe('commitsContainer', () => {
   const commitListener = spy();
   const getLastUpdate: () => TimelineState = () => commitListener.lastCall.args[0];
 
-  beforeEach(() => {
-    commitsContainer.reset();
-    commitListener.resetHistory();
-
-    commitsContainer.addListener(commitListener);
-  });
-
   interface CountState {
     count: number;
-  }
-
-  class Container1 extends StateContainer<CountState> {
-    doStuff() {
-      this.setState({ count: 1 });
-    }
-  }
-
-  class Container2 extends StateContainer<CountState> {
-    doStuff() {
-      this.setState({ count: 2 });
-    }
   }
 
   class CounterContainer extends StateContainer<CountState> {
@@ -39,7 +20,26 @@ describe('commitsContainer', () => {
     }
   }
 
+  beforeEach(() => {
+    commitsContainer.reset();
+    commitListener.resetHistory();
+
+    commitsContainer.addListener(commitListener);
+  });
+
   it('should make a commit for every state update', () => {
+    class Container1 extends StateContainer<CountState> {
+      doStuff() {
+        this.setState({ count: 1 });
+      }
+    }
+
+    class Container2 extends StateContainer<CountState> {
+      doStuff() {
+        this.setState({ count: 2 });
+      }
+    }
+
     const container1 = new Container1();
     const container2 = new Container2();
 
@@ -80,78 +80,15 @@ describe('commitsContainer', () => {
     expect(lastUpdate.branches[0][1].parent).to.equal(lastUpdate.branches[0][0]);
   });
 
-  it('should allow a previous state to be checked out', () => {
+  it('should make commits referentially comparable', () => {
     const container = new CounterContainer();
     container.increment();
+
+    const commit = getLastUpdate().branches[0][0];
+
     container.increment();
 
-    const firstCommit = getLastUpdate().branches[0][0];
-    firstCommit.checkout();
-
-    expect(container.state.count).to.equal(2);
-  });
-
-  it('should checkout a whole range across containers', () => {
-    const container1 = new CounterContainer();
-    const container2 = new CounterContainer();
-
-    container1.increment();
-    container2.increment();
-    container1.increment();
-    container2.increment();
-    container1.increment();
-    container2.increment();
-
-    const commit: StateCommit = getLastUpdate().branches[0][2];
-    commit.checkout();
-
-    expect(container1.state.count).to.equal(3);
-    expect(container2.state.count).to.equal(2);
-  });
-
-  it('should not commit a checkout', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
-
-    const firstCommit = getLastUpdate().branches[0][0];
-
-    commitListener.resetHistory();
-    firstCommit.checkout();
-
-    expect(getLastUpdate().branches[0]).to.have.length(2);
-  });
-
-  it('after a checkout it should record new commits in a new branch', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
-
-    const firstCommit: StateCommit = getLastUpdate().branches[0][0];
-
-    commitListener.resetHistory();
-
-    firstCommit.checkout();
-
-    expect(container.state.count).to.equal(2);
-    container.increment();
-    expect(container.state.count).to.equal(3);
-
-    const lastUpdate = getLastUpdate();
-
-    expect(lastUpdate.branches).to.have.length(2);
-    expect(lastUpdate.branches[1][0].state).to.deep.equal({ count: 3 });
-  });
-
-  it('should update the head when checking out', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
-
-    const firstCommit: StateCommit = getLastUpdate().branches[0][0];
-    firstCommit.checkout();
-
-    expect(getLastUpdate().head).to.equal(firstCommit);
+    expect(getLastUpdate().branches[0][0]).to.equal(commit);
   });
 
   it('should update the active branch when checking out', () => {
@@ -168,69 +105,136 @@ describe('commitsContainer', () => {
     expect(getLastUpdate().activeBranch).to.equal(0);
   });
 
-  it('should update the active branch when checking out the first commit of a diff branch', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
+  describe('checking out', () => {
+    it('should allow a previous state to be checked out', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
 
-    getLastUpdate().branches[0][0].checkout();
+      const firstCommit = getLastUpdate().branches[0][0];
+      firstCommit.checkout();
 
-    container.increment();
+      expect(container.state.count).to.equal(2);
+    });
 
-    getLastUpdate().branches[0][0].checkout();
-    getLastUpdate().branches[1][0].checkout();
+    it('should checkout a whole range across containers', () => {
+      const container1 = new CounterContainer();
+      const container2 = new CounterContainer();
 
-    expect(getLastUpdate().activeBranch).to.equal(1);
+      container1.increment();
+      container2.increment();
+      container1.increment();
+      container2.increment();
+      container1.increment();
+      container2.increment();
+
+      const commit: StateCommit = getLastUpdate().branches[0][2];
+      commit.checkout();
+
+      expect(container1.state.count).to.equal(3);
+      expect(container2.state.count).to.equal(2);
+    });
+
+    it('should not commit a checkout', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
+
+      const firstCommit = getLastUpdate().branches[0][0];
+
+      commitListener.resetHistory();
+      firstCommit.checkout();
+
+      expect(getLastUpdate().branches[0]).to.have.length(2);
+    });
+
+    it('should not replay the entire commit range when checking out an early commit', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
+      container.increment();
+
+      const listener = spy();
+      container.addListener(listener);
+
+      getLastUpdate().branches[0][0].checkout();
+
+      expect(listener).to.have.been.calledOnce;
+    });
+
+    it('should update the head when checking out', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
+
+      const firstCommit: StateCommit = getLastUpdate().branches[0][0];
+      firstCommit.checkout();
+
+      expect(getLastUpdate().head).to.equal(firstCommit);
+    });
+
+    it('should continue the active branch if checking out the head', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
+
+      getLastUpdate().branches[0][0].checkout();
+      getLastUpdate().branches[0][1].checkout();
+
+      container.increment();
+
+      expect(getLastUpdate().branches).to.have.length(1);
+      expect(getLastUpdate().branches[0]).to.have.length(3);
+    });
   });
 
-  it('should create a new branch from the currently checked out commit', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
+  describe('branching', () => {
+    it('after a checkout it should record new commits in a new branch', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
 
-    getLastUpdate().branches[0][0].checkout();
-    container.increment();
+      const firstCommit: StateCommit = getLastUpdate().branches[0][0];
 
-    const lastUpdate = getLastUpdate();
-    expect(lastUpdate.branches[1][0].parent).to.equal(lastUpdate.branches[0][0]);
-  });
+      commitListener.resetHistory();
 
-  it('should not replay the entire commit range when checking out an early commit', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
-    container.increment();
+      firstCommit.checkout();
 
-    const listener = spy();
-    container.addListener(listener);
+      expect(container.state.count).to.equal(2);
+      container.increment();
+      expect(container.state.count).to.equal(3);
 
-    getLastUpdate().branches[0][0].checkout();
+      const lastUpdate = getLastUpdate();
 
-    expect(listener).to.have.been.calledOnce;
-  });
+      expect(lastUpdate.branches).to.have.length(2);
+      expect(lastUpdate.branches[1][0].state).to.deep.equal({ count: 3 });
+    });
 
-  it('should make commits referentially comparable', () => {
-    const container = new CounterContainer();
-    container.increment();
+    it('should create a new branch from the currently checked out commit', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
 
-    const commit = getLastUpdate().branches[0][0];
+      getLastUpdate().branches[0][0].checkout();
+      container.increment();
 
-    container.increment();
+      const lastUpdate = getLastUpdate();
+      expect(lastUpdate.branches[1][0].parent).to.equal(lastUpdate.branches[0][0]);
+    });
 
-    expect(getLastUpdate().branches[0][0]).to.equal(commit);
-  });
+    it('should update the active branch when checking out the first commit of a diff branch', () => {
+      const container = new CounterContainer();
+      container.increment();
+      container.increment();
 
-  it('should continue the active branch if checking out the head', () => {
-    const container = new CounterContainer();
-    container.increment();
-    container.increment();
+      getLastUpdate().branches[0][0].checkout();
 
-    getLastUpdate().branches[0][0].checkout();
-    getLastUpdate().branches[0][1].checkout();
+      container.increment();
 
-    container.increment();
+      getLastUpdate().branches[0][0].checkout();
+      getLastUpdate().branches[1][0].checkout();
 
-    expect(getLastUpdate().branches).to.have.length(1);
-    expect(getLastUpdate().branches[0]).to.have.length(3);
+      expect(getLastUpdate().activeBranch).to.equal(1);
+    });
   });
 });
