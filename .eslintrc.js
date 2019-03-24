737e55b00d2c81ee695c0988c60f15f84cf7ee0b
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-24 16:31:52 +0100

    Upgrade to the latest ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   'root': true,
 
-  'parser': 'typescript-eslint-parser',
-
-  'plugins': [
-    'typescript'
-  ],
-
   'extends': [
-    '@nighttrax/eslint-config/tsx'
+    '@nighttrax/eslint-config-tsx'
   ]
 };
